8dd5ea9d464882c899a947f3e91b43ddfab533b7
Ivan Husarov <ivan.husarov.mp.2018@lpnu.ua>
2020-09-25 01:27:50 +0300

    image upload

---
+++ src/main/java/com/inmost/bulletin_board/Configuration/StaticResourceConfiguration.java
@@ -0,0 +1,28 @@
+package com.inmost.bulletin_board.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+
+@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+
+    @Value("${path.imageFolder}")
+    private String source;
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+                .addResourceLocations("file:///"+ Path.of(source).toAbsolutePath().toString())
+                .setCacheControl(CacheControl.noCache());
+    }
+}
